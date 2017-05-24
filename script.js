6306a54b5ac7636c8dd3005807b49080a92f5a81
Chris Boeckel <cboe07@gmail.com>
2017-05-23 20:58:12 -0400

    Add API and objects

--- script.js
+++ script.js
@@ -20,12 +20,49 @@ $(document).ready(()=>{
 				curr: weatherData.main.temp,
 				max: weatherData.main.temp_max,
 				min: weatherData.main.temp_min,
+				hum: weatherData.main.humidity,
+				winSpeed: weatherData.wind.speed,
+				winDir: weatherData.wind.deg,
+				rise: weatherData.sys.sunrise,
+				set: weatherData.sys.sunset,
+				cloud: weatherData.clouds.all,
+				
 			}
 			var name = weatherData.name;
 			var icon = weatherData.weather[0].icon + '.png';
 			var desc = weatherData.weather[0].description;
-			var newHTML = '<img src="http://openweathermap.org/img/w/' + icon+'">' + desc;
-			newHTML += '<div>The temp in ' + name + ' is currently ' + currTemp + '&deg;</div>';
+
+			var secRise = temps.rise;
+			var secSet = temps.set;
+			var secRiseDate = new Date(secRise * 1000);
+			var secSetDate = new Date(secSet * 1000);
+			var riseTimeStr = secRiseDate.toLocaleTimeString();
+			var setTimeStr = secSetDate.toLocaleTimeString();
+			// console.log(secRiseDate,riseTimeStr);
+
+			// $("#d1").html(date);
+			// $("#d2").html(timestr);
+
+
+			var newHTML = '<img src="http://openweathermap.org/img/w/' + icon +' ">' + desc;
+			// newHTML += '<div>The temp in ' + name + ' is currently ' + currTemp + '&deg;</div>';
+			
+			newHTML += '<h2>' + name + '</h2>'
+			newHTML += '<br>'
+			newHTML += '<div>Current Temperature:  ' + temps.curr + '&deg;</div>';
+            newHTML += '<div>High:  ' + temps.max + '&deg;</div>';
+            newHTML += '<div>Low:  ' + temps.min + '&deg;</div>';
+            newHTML += '<br>'
+            newHTML += '<div>Humidity:  ' + temps.hum + '&#37;</div>';
+            newHTML += '<br>'
+            newHTML += '<div>Wind Speed:  ' + temps.winSpeed + 'mph</div>';
+            newHTML += '<div>Wind Direction:  ' + temps.winDir + '&deg</div>';
+            newHTML += '<br>'
+            newHTML += '<div>Sunrise:  ' + riseTimeStr + '</div>';
+            newHTML += '<div>Sunset:  ' + setTimeStr + '</div>';
+            newHTML += '<br>'
+            newHTML += '<div>Cloudiness:  ' + temps.cloud + '%</div>';
+
 			$('#temp-info').html(newHTML);
 			currentPercent = 0;
 			animateCircle(0,currTemp);
@@ -38,22 +75,28 @@ $(document).ready(()=>{
 	var assumedTemperature = 65;
 	var currentPercent = 0;
 
+
 	function animateCircle(currentArc,currentTemp){
+
 		// console.log(currentArc);
 		// Draw Inner Circle
-		context.fillStyle = "#ccc";
+		context.fillStyle = "#053c5e";
 		context.beginPath();
-		context.arc(155,75,70,Math.PI*0,Math.PI*2);
+		context.arc(155,105,90,Math.PI*0,Math.PI*2);
 		context.closePath();
 		context.fill();
 
 		// Draw the outter line
 		// 5px wide line
-		context.lineWidth = 5; 
-		context.strokeColor = '#ffff00';
+		context.lineWidth = 15; 
+		context.strokeStyle = '#fcba04';
 		context.beginPath();
-		context.arc(155,75,75,Math.PI*1.5,(Math.PI * 2 * currentArc) + Math.PI*1.5);
+		context.arc(155,105,95,Math.PI*1.5,(Math.PI * 2 * currentArc) + Math.PI*1.5);
 		context.stroke();
+		// context.font = "48px Myriad Pro";
+  //       context.fillStyle = "fcba04";
+  //       context.textBaseline = "top";
+  //       context.fillText(temps.curr,155, 105);
 
 		// Update the current Perecentage
 		currentPercent++;
