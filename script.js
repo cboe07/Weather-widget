8c423c0c14c766a2c075a7492c7159bb9e1b19c0
Chris Boeckel <cboe07@gmail.com>
2017-05-26 13:00:22 -0400

    Refactor

--- script.js
+++ script.js
@@ -38,7 +38,10 @@ $(document).ready(()=>{
 			var secSetDate = new Date(secSet * 1000);
 			var riseTimeStr = secRiseDate.toLocaleTimeString();
 			var setTimeStr = secSetDate.toLocaleTimeString();
-			// console.log(secRiseDate,riseTimeStr);
+			console.log(secRiseDate,riseTimeStr);
+
+			var time = new Date().toTimeString().split(" ")[0];
+			console.log(time);
 
 			// $("#d1").html(date);
 			// $("#d2").html(timestr);
@@ -49,9 +52,9 @@ $(document).ready(()=>{
 			
 			newHTML += '<h2>' + name + '</h2>'
 			newHTML += '<br>'
-			newHTML += '<div>Current Temperature:  ' + temps.curr + '&deg;</div>';
-            newHTML += '<div>High:  ' + temps.max + '&deg;</div>';
-            newHTML += '<div>Low:  ' + temps.min + '&deg;</div>';
+			newHTML += '<div>Current Temperature:  ' + temps.curr + '&degF;</div>';
+            newHTML += '<div>High:  ' + temps.max + '&degF;</div>';
+            newHTML += '<div>Low:  ' + temps.min + '&degF;</div>';
             newHTML += '<br>'
             newHTML += '<div>Humidity:  ' + temps.hum + '&#37;</div>';
             newHTML += '<br>'
@@ -66,7 +69,25 @@ $(document).ready(()=>{
 			$('#temp-info').html(newHTML);
 			currentPercent = 0;
 			animateCircle(0,currTemp);
+
+			
+
+			function sunRiseSet(){
+				// console.log(riseTimeStr);
+				context.stroke();
+				context.font = "10px Khula";
+				context.fillStyle = 'rgba(252,186,04,0.6)';
+				// contextBaseline = "top";
+				context.fillText(riseTimeStr,10,90);
+			}
+			sunRiseSet();
+	
+
+
+
+			
 		});
+
 	});
 
 
@@ -77,10 +98,10 @@ $(document).ready(()=>{
 
 
 	function animateCircle(currentArc,currentTemp){
-
+		context.clearRect(0,0,500,500);
 		// console.log(currentArc);
 		// Draw Inner Circle
-		context.fillStyle = "#053c5e";
+		context.fillStyle = 'rgba(5,60,94,0.6)';
 		context.beginPath();
 		context.arc(155,105,90,Math.PI*0,Math.PI*2);
 		context.closePath();
@@ -89,7 +110,7 @@ $(document).ready(()=>{
 		// Draw the outter line
 		// 5px wide line
 		context.lineWidth = 15; 
-		context.strokeStyle = '#fcba04';
+		context.strokeStyle = 'rgba(252,186,04,0.6)';
 		context.beginPath();
 		context.arc(155,105,95,Math.PI*1.5,(Math.PI * 2 * currentArc) + Math.PI*1.5);
 		context.stroke();
@@ -98,6 +119,14 @@ $(document).ready(()=>{
   //       context.textBaseline = "top";
   //       context.fillText(temps.curr,155, 105);
 
+		if (currentTemp != undefined){                             //<<<<<<<===============puts temp in circle
+			context.stroke();
+			context.font = "30px Khula";
+			context.fillStyle = 'rgba(252,186,04,0.6)';
+			context.textBaseline = "top";
+			context.fillText(currentTemp+String.fromCharCode(176)+'F',110,90);
+		}
+
 		// Update the current Perecentage
 		currentPercent++;
 		if(currentPercent < currentTemp){
@@ -105,7 +134,52 @@ $(document).ready(()=>{
 				animateCircle(currentPercent/100,currentTemp);
 			});
 		}
+		animateSun();
+		// sunRiseSet();
+
+		
+		
+
 	}
 	// animateCircle();
+
+	function animateSun(){
+		context.lineWidth = 2;
+		context.strokeStyle = 'black';
+		context.beginPath();
+		context.moveTo(60, 405);
+		context.bezierCurveTo(85,305,235,305,260,405);
+		context.stroke();
+
+		// context.fillStyle = '#fcba04';
+		// context.beginPath();
+		// context.arc(60,405,10,Math.PI*0,Math.PI*2);
+		// context.closePath();
+		// context.fill();
+		// requestAnimationFrame(function(){
+		// 	animateSun(currentPercent/100,currentTemp);
+		// });	
+		function makeSun(){
+			var sunImage = new Image();
+			sunImage.src = 'images/sun.png';
+			context.drawImage(sunImage,15,375);
+
+		}	
+		makeSun();
+		
+
+		
+		
+	}
+	// function sunRiseSet(){
+	// 		context.stroke();
+	// 		context.font = "10px Khula";
+	// 		context.fillStyle = 'rgba(252,186,04,0.6)';
+	// 		contextBaseline = "top";
+	// 		context.fillText(riseTimeStr,10,90);
+	// }
+	
+
+	
 
 });
